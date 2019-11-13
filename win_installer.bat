1b40f99bb850ac026ff333471583bde3749a4030
LunziQwQ <me@lunzi.space>
2019-11-13 18:59:59 +0800

    fix installer.bat

--- win_installer.bat
+++ win_installer.bat
@@ -2,8 +2,8 @@
 ECHO 欢迎安装KylinDealer
 ECHO 即将弹出依赖库安装，请在弹出窗口中，勾选下方的两个对勾并点击安装
 PAUSE
-START python_installer.exe
+python_installer.exe
 ECHO 安装Python完成，下一步，安装依赖库
-START pip install --user -r requirements.txt
+pip install --user -r requirements.txt
 ECHO 安装完成！
 PAUSE
